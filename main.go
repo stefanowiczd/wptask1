49840315f088de02ff0d38eede827c912013d99e
Dominik Stefanowicz <dominik.stefanowicz@gmail.com>
2020-10-01 23:36:56 +0200

    error cleanup

--- main.go
+++ main.go
@@ -22,40 +22,10 @@ var (
 	contextKeyID = contextKey("id")
 )
 
-// ErrResponse is a type to represent API errors and corresponding message
-type ErrResponse struct {
-	Err            error `json:"-"` // low-level runtime error
-	HTTPStatusCode int   `json:"-"` // http response status code
-
-	StatusText string `json:"status"`          // user-level status message
-	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
-	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
-}
-
-func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
-	render.Status(r, e.HTTPStatusCode)
-	return nil
-}
-
-func ErrInvalidRequest(err error) render.Renderer {
-	return &ErrResponse{
-		Err:            err,
-		HTTPStatusCode: 400,
-		StatusText:     "Invalid request.",
-		ErrorText:      err.Error(),
-	}
-}
-
-func ErrRender(err error) render.Renderer {
-	return &ErrResponse{
-		Err:            err,
-		HTTPStatusCode: 422,
-		StatusText:     "Error rendering response.",
-		ErrorText:      err.Error(),
-	}
-}
-
+// ErrNotFound error not found
 var ErrNotFound = &ErrResponse{HTTPStatusCode: 404, StatusText: "Resource not found."}
+
+// ErrNoError no error
 var ErrNoError = &ErrResponse{HTTPStatusCode: 200, StatusText: "OK"}
 
 func main() {
